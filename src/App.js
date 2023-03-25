c6e0539ce0116a57420befa9ed000ea2a64907f2
kdy <kkykdy4334@gmail.com>
2023-03-25 19:43:11 +0900

    feat: make real assets page

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ import '@fontsource/shrikhand'; // title TODO: refactoring 필요
 import '@fontsource/roboto-condensed'; // subtitle
 import Page404 from '@pages/page404/Page404';
 import NFTRafflesPage from '@pages/NFTRafflesPage/NFTRafflesPage';
-import RealAssetsRafflesPage from '@pages/realAssetsRafflesPage/RealAssetsRafflesPage';
 import RaffleDetailPage from '@pages/raffleDetailPage/RaffleDetailPage';
+import RealAssetsRafflesPage from '@pages/realAssetsRafflesPage/RealAssetsRafflesPage';
+import RealAssetsDetailPage from '@pages/realAssetsDetailPage/RealAssetsDetailPage';
 import CreateNewRafflePage from '@pages/createNewRafflePage/CreateNewRafflePage';
 import MyPage from '@pages/myPage/MyPage';
 import CreateNewNFTPage from '@pages/createNewNFTPage/CreateNewNFTPage';
@@ -24,11 +25,12 @@ function App() {
       <BrowserView>
         <Routes>
           <Route path="/" element={<NFTRafflesPage />} />
+          <Route path="/raffleDetail" element={<RaffleDetailPage />} />
           <Route
             path="/realAssetsRaffles"
             element={<RealAssetsRafflesPage />}
           />
-          <Route path="/raffleDetail" element={<RaffleDetailPage />} />
+          <Route path="/realAssetsDetail" element={<RealAssetsDetailPage />} />
           <Route path="/createRaffle" element={<CreateNewRafflePage />} />
           <Route path="/my" element={<MyPage />} />
           <Route path="/createNFT" element={<CreateNewNFTPage />} />
